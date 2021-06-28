a6f2acd06fd197b793c5c04124efc4eeba8e4ffd
SahilB689 <76981086+SahilB689@users.noreply.github.com>
2021-06-28 14:25:16 -0400

    Update settings.py

--- project3/settings.py
+++ project3/settings.py
@@ -122,3 +122,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')  
+
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
+    
